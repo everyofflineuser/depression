b5c9849de565f4d31cdc61f484d75c960e3c49d3
ВсегдаВыключенныйПользователь <nealeks18@mail.ru>
2024-10-29 00:15:24 +0900

    Revert "Merge branch 'master' of https://github.com/everyofflineuser/depression"
    This reverts commit 25167681f0b77c634acf65de2833080d2ebe7d86, reversing
    changes made to d7c8e69cce25c6ccaeddc88db10ff977db71fb33.

--- Network/MessageReceiver.cs
+++ Network/MessageReceiver.cs
@@ -1,5 +1,4 @@
 ﻿using System.Numerics;
-using CopperDevs.Logger;
 using depression.Entities;
 using depression.Extensions;
 using depression.Managers;
@@ -19,9 +18,7 @@ public static class MessageReceiver
         Quaternion rotation = message.GetQuaternion();
         ushort entityId = message.GetUShort();
 
-        if (SceneManager.ActiveScene?.GetEntity(entityId) == null) return;
-        
-        if (!NetworkManager.IsNetworkEntity(SceneManager.ActiveScene.GetEntity(entityId)))
+        if (!NetworkManager.IsNetworkEntity(SceneManager.ActiveScene?.GetEntity(entityId)!))
         {
             Message error = Message.Create(MessageSendMode.Reliable, MessageId.Error)
                 .AddString("Bruh, is it a network entity?");
@@ -43,8 +40,6 @@ public static class MessageReceiver
     [MessageHandler((ushort)MessageId.Sync)]
     private static void SyncRequestServer(ushort fromClientId, Message message)
     {
-        MessageExtensions.SendAllEntitiesToClient(fromClientId);
-        
         foreach (NetworkEntity entity in NetworkManager.GetNetworkEntities())
         {
             Message entityUpdate = Message.Create(MessageSendMode.Reliable, MessageId.EntityUpdate)
@@ -57,72 +52,15 @@ public static class MessageReceiver
         }
     }
     
-    [MessageHandler((ushort)MessageId.SyncSpecifiedId)]
-    private static void SyncRequestSpecifiedIdServer(ushort fromClientId, Message message)
-    {
-        MessageExtensions.SendEntityToClient(fromClientId, message.GetUShort());
-    }
-    
     [MessageHandler((ushort)MessageId.EntityUpdate)]
     private static void EntityUpdateClient(Message message)
     {
-        // Проверяем, достаточно ли данных в сообщении
-        if (message.UnreadBits < 4) // Например, если ожидается хотя бы 4 бита для ID
-        {
-            RiptideLogger.Log(LogType.Warning, "Недостаточно данных в сообщении для обновления сущности.");
-            return;
-        }
-
         Vector3 position = message.GetVector3();
         Vector3 scale = message.GetVector3();
         Quaternion rotation = message.GetQuaternion();
         ushort entityId = message.GetUShort();
 
-        // Проверяем, существует ли сущность перед обновлением
-        var entity = SceneManager.ActiveScene?.GetEntity(entityId);
-        if (entity == null)
-        {
-            Log.Warning($"Сущность с ID {entityId} не найдена. Возможно, она еще не была создана.");
-            
-            Message syncRequestSpecifiedId = Message.Create(MessageSendMode.Reliable, MessageId.SyncSpecifiedId)
-                .AddUShort(entityId);
-            
-            NetworkManager.CurrentClient!.Send(syncRequestSpecifiedId);
-            
-            return; // Выход из метода, если сущность не найдена
-        }
-
         NetworkManager.UpdateEntity(entityId, position, scale, rotation);
-    }
-    
-    [MessageHandler((ushort)MessageId.EntityCreate)]
-    private static void EntityCreateClient(Message message)
-    {
-        string typeName = message.GetString(); // Получаем имя типа
-        Vector3 position = message.GetVector3();
-        Vector3 scale = message.GetVector3();
-        Quaternion rotation = message.GetQuaternion();
-
-        // Получаем тип из имени
-        Type entityType = Type.GetType(typeName);
-        if (entityType == null)
-        {
-            // Обработка ошибки, если тип не найден
-            Log.Warning($"Тип {typeName} не найден.");
-            return;
-        }
-
-        // Создаем экземпляр конкретного типа
-        NetworkEntity entity = (NetworkEntity)Activator.CreateInstance(entityType);
-        MessageExtensions.DeserializeEntityProperties(entity, message);
-        
-        SceneManager.ActiveScene!.AddEntity(entity);
-        
-        entity.Position = position;
-        entity.Scale = scale;
-        entity.Rotation = rotation;
-        
-        entity.InitSynced = true;
     }
 
     [MessageHandler((ushort)MessageId.Error)]
